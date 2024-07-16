4467d02eb7ff1d204258fbb0e6884305fb2b61cc
DaisukeMatsuura <mail@brightful.jp>
2024-07-17 01:05:53 +0900

    feat: Add API routes for testing and retrieving users

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/test', function () {
+    return response()->json(['message' => 'Hello World!'], 200);
+});
+
+Route::get('/users', function ($name) {
+    $users = User::all();
+    return response()->json($users, 200);
 });
