5c8af7d51571fc2730bb429da34bac4f6cef278b
DaisukeMatsuura <mail@brightful.jp>
2024-07-17 12:03:10 +0900

    chore: reset

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
 
-Route::get('/test', function (Request $request) {
-    return response()->json(['message' => 'Hello World!']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
